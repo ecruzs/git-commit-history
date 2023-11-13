5202860b32d1296c60b8952f38a13e82cb3c7176
ecruzs <ecruz.icc@gmail.com>
2023-11-13 10:02:31 -0300

    Delete backend/dist directory

--- backend/dist/modules/commits/commits.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { CommitsService } from './commits.service';
-export declare class CommitsController {
-    private githubService;
-    constructor(githubService: CommitsService);
-    getCommits(params: any): Promise<any>;
-}
--- backend/dist/modules/commits/commits.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class CommitsService {
-    private httpService;
-    constructor(httpService: HttpService);
-    getCommits(repo: string): Promise<any>;
-}
